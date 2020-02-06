3efd212843a5c0aed7ffbe9dd23e3e0ec3a66ecc
Raj <rajshah@outlook.ie>
2020-02-06 19:13:20 +0000

    video reader bug

--- tools/video_net.py
+++ tools/video_net.py
@@ -2,7 +2,6 @@ import os, sys
 from time import time
 
 import numpy as np
-import pandas as pd
 import cv2
 import torch
 
@@ -20,6 +19,8 @@ from slowfast.datasets.cv2_transform import scale
 
 logger = logging.get_logger(__name__)
 np.random.seed(20)
+
+LABELS_INTERESTED_IN = [51, 57, 63, 65, 69, 70, 72, 75]
 
 
 class VideoReader(object):
@@ -45,16 +46,10 @@ class VideoReader(object):
     def __next__(self):
         was_read, frame = self.cap.read()
         if not was_read:
-            # raise StopIteration
-            ## reiterate the video instead of quiting.
-            self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-            frame = None
+            self.cap.release()
+            raise StopIteration
 
         return was_read, frame
-
-    def clean(self):
-        self.cap.release()
-        cv2.destroyAllWindows()
 
 
 def video(cfg):
@@ -74,14 +69,7 @@ def video(cfg):
     misc.log_model_info(model)
 
     # Load a checkpoint to test if applicable.
-    if cfg.TEST.CHECKPOINT_FILE_PATH != "":
-        ckpt = cfg.TEST.CHECKPOINT_FILE_PATH
-    elif cu.has_checkpoint(cfg.OUTPUT_DIR):
-        ckpt = cu.get_last_checkpoint(cfg.OUTPUT_DIR)
-    elif cfg.TRAIN.CHECKPOINT_FILE_PATH != "":
-        # If no checkpoint found in TEST.CHECKPOINT_FILE_PATH or in the current
-        # checkpoint folder, try to load checkpoint from
-        # TRAIN.CHECKPOINT_FILE_PATH and test it.
+    if cfg.TRAIN.CHECKPOINT_FILE_PATH != "":
         ckpt = cfg.TRAIN.CHECKPOINT_FILE_PATH
     else:
         raise NotImplementedError("Unknown way to load checkpoint.")
@@ -89,30 +77,28 @@ def video(cfg):
     cu.load_checkpoint(ckpt, model, cfg.NUM_GPUS > 1, None, inflation=False,
                        convert_from_caffe2="caffe2" in [cfg.TEST.CHECKPOINT_TYPE, cfg.TRAIN.CHECKPOINT_TYPE])
 
-    if cfg.DETECTION.ENABLE:
-        # Load object detector from detectron2
-        dtron2_cfg_file = cfg.VIDEO_DEMO.DETECTRON2_OBJECT_DETECTION_MODEL_CFG
-        dtron2_cfg = get_cfg()
-        dtron2_cfg.merge_from_file(model_zoo.get_config_file(dtron2_cfg_file))
-        dtron2_cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = .5
-        dtron2_cfg.MODEL.WEIGHTS = cfg.VIDEO_DEMO.DETECTRON2_OBJECT_DETECTION_MODEL_WEIGHTS
-        object_predictor = DefaultPredictor(dtron2_cfg)
-        # Load the labels of AVA dataset
-        with open(cfg.VIDEO_DEMO.LABEL_FILE_PATH) as f:
-            labels = f.read().split('\n')[:-1]
-        palette = np.random.randint(64, 128, (len(labels), 3)).tolist()
-        boxes = []
-    else:
-        # Load the labels of Kinectics-400 dataset
-        labels_df = pd.read_csv(cfg.VIDEO_DEMO.LABEL_FILE_PATH)
-        labels = labels_df['name'].values
+    # Load object detector from detectron2
+    dtron2_cfg_file = cfg.VIDEO_DEMO.DETECTRON2_OBJECT_DETECTION_MODEL_CFG
+    dtron2_cfg = get_cfg()
+    dtron2_cfg.merge_from_file(model_zoo.get_config_file(dtron2_cfg_file))
+    dtron2_cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = .5
+    dtron2_cfg.MODEL.WEIGHTS = cfg.VIDEO_DEMO.DETECTRON2_OBJECT_DETECTION_MODEL_WEIGHTS
+    object_predictor = DefaultPredictor(dtron2_cfg)
+    # Load the labels of AVA dataset
+    with open(cfg.VIDEO_DEMO.LABEL_FILE_PATH) as f:
+        labels = f.read().split('\n')[:-1]
+    palette = np.random.randint(64, 128, (len(labels), 3)).tolist()
+    boxes = []
 
     frame_provider = VideoReader(cfg)
     seq_len = cfg.DATA.NUM_FRAMES * cfg.DATA.SAMPLING_RATE
     frames = []
     pred_labels = []
-    # s = 0.
-    for idx, (able_to_read, frame) in enumerate(frame_provider):
+
+    # main start
+    main_start = time()
+
+    for able_to_read, frame in frame_provider:
         if not able_to_read:
             # when reaches the end frame, clear the buffer and continue to the next one.
             frames = []
@@ -126,7 +112,7 @@ def video(cfg):
                 mid_frame = frame
 
         if len(frames) == seq_len:
-            start = time()
+
             if cfg.DETECTION.ENABLE:
                 outputs = object_predictor(mid_frame)
                 fields = outputs["instances"]._fields
@@ -173,47 +159,49 @@ def video(cfg):
                 inputs = inputs.cuda(non_blocking=True)
 
             # Perform the forward pass.
-            if cfg.DETECTION.ENABLE:
-                # When there is nothing in the scene,
-                #   use a dummy variable to disable all computations below.
-                if not len(boxes):
-                    preds = torch.tensor([])
-                else:
-                    preds = model(inputs, boxes)
+            # When there is nothing in the scene,
+            #   use a dummy variable to disable all computations below.
+            if not len(boxes):
+                preds = torch.tensor([])
             else:
-                preds = model(inputs)
+                preds = model(inputs, boxes)
 
             # Gather all the predictions across all the devices to perform ensemble.
             if cfg.NUM_GPUS > 1:
                 preds = du.all_gather(preds)[0]
 
             if cfg.DETECTION.ENABLE:
-                # This post processing was intendedly assigned to the cpu since my laptop GPU
-                #   RTX 2080 runs out of its memory, if your GPU is more powerful, I'd recommend
-                #   to change this section to make CUDA does the processing.
+                # if GPU is more powerful, change to CUDA processing.
                 preds = preds.cpu().detach().numpy()
-                pred_masks = preds > .1
+                pred_masks = preds > 0.1
                 label_ids = [np.nonzero(pred_mask)[0] for pred_mask in pred_masks]
+
+                # filter labels according to LABELS_INTERESTED_IN
+                new_label_ids = [[x for x in sublist if x in LABELS_INTERESTED_IN] for sublist in label_ids]
+
                 pred_labels = [
                     [labels[label_id] for label_id in perbox_label_ids]
-                    for perbox_label_ids in label_ids
+                    for perbox_label_ids in new_label_ids
                 ]
-                # I'm unsure how to detectron2 rescales boxes to image original size, so I use
-                #   input boxes of slowfast and rescale back it instead, it's safer and even if boxes
-                #   was not rescaled by cv2_transform.rescale_boxes, it still works.
-                boxes = boxes.cpu().detach().numpy()
-                ratio = np.min(
-                    [frame_provider.display_height, frame_provider.display_width]
-                ) / cfg.DATA.TEST_CROP_SIZE
-                boxes = boxes[:, 1:] * ratio
+
+                print(pred_labels)
+
+                # unsure how to detectron2 rescales boxes to image original size, so I use
+                # input boxes of slowfast and rescale back it instead, it's safer and even if boxes
+                # was not rescaled by cv2_transform.rescale_boxes, it still works.
+                # boxes = boxes.cpu().detach().numpy()
+                # ratio = np.min(
+                #     [frame_provider.display_height, frame_provider.display_width]
+                # ) / cfg.DATA.TEST_CROP_SIZE
+                # boxes = boxes[:, 1:] * ratio
             else:
                 ## Option 1: single label inference selected from the highest probability entry.
                 # label_id = preds.argmax(-1).cpu()
                 # pred_label = labels[label_id]
                 # Option 2: multi-label inferencing selected from probability entries > threshold
-                label_ids = torch.nonzero(preds.squeeze() > .1).reshape(-1).cpu().detach().numpy()
+                label_ids = torch.nonzero(preds.squeeze() > 0.1).reshape(-1).cpu().detach().numpy()
                 pred_labels = labels[label_ids]
-                logger.info(pred_labels)
+                # logger.info(pred_labels)
                 if not list(pred_labels):
                     pred_labels = ['Unknown']
 
@@ -221,49 +209,26 @@ def video(cfg):
             # frames.pop(0)
             # option 2: empty the buffer
             frames = []
-            # s = time() - start
 
-        if cfg.DETECTION.ENABLE and pred_labels and boxes.any():
-            for box, box_labels in zip(boxes.astype(int), pred_labels):
-                cv2.rectangle(frame, tuple(box[:2]), tuple(box[2:]), (0, 255, 0), thickness=2)
-                label_origin = box[:2]
-                for label in box_labels:
-                    label_origin[-1] -= 5
-                    (label_width, label_height), _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, .5, 2)
-                    cv2.rectangle(
-                        frame,
-                        (label_origin[0], label_origin[1] + 5),
-                        (label_origin[0] + label_width, label_origin[1] - label_height - 5),
-                        palette[labels.index(label)], -1
-                    )
-                    cv2.putText(
-                        frame, label, tuple(label_origin),
-                        cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1
-                    )
-                    label_origin[-1] -= label_height + 5
-        if not cfg.DETECTION.ENABLE:
-            # Display predicted labels to frame.
-            y_offset = 50
-            cv2.putText(frame, 'Action:', (10, y_offset),
-                        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                        fontScale=.65, color=(0, 235, 0), thickness=2)
-            for pred_label in pred_labels:
-                y_offset += 30
-                cv2.putText(frame, '{}'.format(pred_label), (20, y_offset),
-                            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                            fontScale=.65, color=(0, 235, 0), thickness=2)
+        # if cfg.DETECTION.ENABLE and pred_labels and boxes.any():
+        #     for box, box_labels in zip(boxes.astype(int), pred_labels):
+        #         cv2.rectangle(frame, tuple(box[:2]), tuple(box[2:]), (0, 255, 0), thickness=2)
+        #         label_origin = box[:2]
+        #         for label in box_labels:
+        #             label_origin[-1] -= 5
+        #             (label_width, label_height), _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, .5, 2)
+        #             cv2.rectangle(
+        #                 frame,
+        #                 (label_origin[0], label_origin[1] + 5),
+        #                 (label_origin[0] + label_width, label_origin[1] - label_height - 5),
+        #                 palette[labels.index(label)], -1
+        #             )
+        #             cv2.putText(
+        #                 frame, label, tuple(label_origin),
+        #                 cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1
+        #             )
+        #             label_origin[-1] -= label_height + 5
 
-        # # Display prediction speed
-        # cv2.putText(frame, 'Speed: {:.2f}s'.format(s), (10, 25),
-        #             fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-        #             fontScale=.65, color=(0, 235, 0), thickness=2)
-        # Display the frame
-        # cv2.imshow('SlowFast', frame)
-        cv2.imwrite(os.path.join(cfg.OUTPUT_DIR, 'frame{}.png'.format(idx)), frame)
 
-        # # hit Esc to quit the demo.
-        # key = cv2.waitKey(1)
-        # if key == 27:
-        #     break
 
-    frame_provider.clean()
+    print('total time: {}'.format(time() - main_start))
